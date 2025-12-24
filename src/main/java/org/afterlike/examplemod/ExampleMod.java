113305857a575dcb2d87edc0d91daf6d7ef63d1e
polaris <dev@afterlike.org>
2025-12-23 19:48:36 -0500

    style: apply spotless formatting

--- src/main/java/org/afterlike/examplemod/ExampleMod.java
+++ src/main/java/org/afterlike/examplemod/ExampleMod.java
@@ -7,13 +7,12 @@ import org.jetbrains.annotations.Nullable;
 public class ExampleMod {
 	private static final @Nullable ExampleMod instance = new ExampleMod();
 	private static final @NotNull String VERSION = org.afterlike.examplemod.BuildConstants.VERSION;
-
 	public ExampleMod() {
 	}
 
-    public static @NotNull ExampleMod get() {
-        return Objects.requireNonNull(instance);
-    }
+	public static @NotNull ExampleMod get() {
+		return Objects.requireNonNull(instance);
+	}
 
 	public @NotNull String getVersion() {
 		return VERSION;
--- src/main/java/org/afterlike/examplemod/platform/ForgeModEntry.java
+++ src/main/java/org/afterlike/examplemod/platform/ForgeModEntry.java
@@ -3,4 +3,5 @@ package org.afterlike.examplemod.platform;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(modid = "examplemod", useMetadata = true)
-public class ForgeModEntry {}
+public class ForgeModEntry {
+}
